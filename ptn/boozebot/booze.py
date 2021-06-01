afda238949d1bcd851380dfdbeb2f590067413f4
gcruics <53232597+gcruicks@users.noreply.github.com>
2021-06-01 13:25:44 +0200

    Track the carriers coming for booze cruise
    Database for booze cruise
    Command to forcefully update the database

--- ptn/boozebot/booze.py
+++ ptn/boozebot/booze.py
@@ -2,9 +2,11 @@ import ast
 import os
 from dotenv import load_dotenv
 
+from ptn.boozebot.commands.DatabaseInteraction import DatabaseInteraction
 from ptn.boozebot.commands.DiscordBotCommands import DiscordBotCommands
 from ptn.boozebot.commands.Unloading import Unloading
 from ptn.boozebot.constants import bot
+from ptn.boozebot.database.database import build_database_on_startup, dump_database
 
 _production = ast.literal_eval(os.environ.get('PTN_BOOZE_BOT', 'False'))
 
@@ -17,6 +19,8 @@ TOKEN = os.getenv('DISCORD_TOKEN_PROD') if _production else os.getenv('DISCORD_T
 print(f'Booze bot is connecting against production: {_production}.')
 
 if __name__ == '__main__':
+    build_database_on_startup()
     bot.add_cog(DiscordBotCommands(bot))
     bot.add_cog(Unloading())
+    bot.add_cog(DatabaseInteraction())
     bot.run(TOKEN)
