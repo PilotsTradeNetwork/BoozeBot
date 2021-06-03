e6e23e9b76faa162e143133ca98f20c8462d4e3d
gcruics <53232597+gcruicks@users.noreply.github.com>
2021-06-03 10:44:42 +0200

    Support multiple runs for the same carrier id
    Track how many runs each carrier does by amalgamating things

--- ptn/boozebot/booze.py
+++ ptn/boozebot/booze.py
@@ -1,20 +1,11 @@
 import ast
 import os
-from dotenv import load_dotenv
 
 from ptn.boozebot.commands.DatabaseInteraction import DatabaseInteraction
 from ptn.boozebot.commands.DiscordBotCommands import DiscordBotCommands
 from ptn.boozebot.commands.Unloading import Unloading
-from ptn.boozebot.constants import bot
-from ptn.boozebot.database.database import build_database_on_startup, dump_database
-
-_production = ast.literal_eval(os.environ.get('PTN_BOOZE_BOT', 'False'))
-
-# Get the discord token from the local .env file. Deliberately not hosted in the repo or Discord takes the bot down
-# because the keys are exposed. DO NOT HOST IN THE REPO. Seriously do not do it ...
-load_dotenv()
-
-TOKEN = os.getenv('DISCORD_TOKEN_PROD') if _production else os.getenv('DISCORD_TOKEN_TESTING')
+from ptn.boozebot.constants import bot, TOKEN, _production
+from ptn.boozebot.database.database import build_database_on_startup
 
 print(f'Booze bot is connecting against production: {_production}.')
 
