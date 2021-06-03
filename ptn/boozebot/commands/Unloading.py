9580f6e2b274f62d022fb9e4af0f4ec92ee27091
gcruics <53232597+gcruicks@users.noreply.github.com>
2021-06-03 09:17:31 +0200

    Updated the commands to hold permissions and choice values

--- ptn/boozebot/commands/Unloading.py
+++ ptn/boozebot/commands/Unloading.py
@@ -1,9 +1,13 @@
 import discord
 from discord.ext import commands
 from discord_slash import SlashContext, cog_ext
+from discord_slash.utils.manage_commands import create_option, create_choice
+from discord_slash.utils.manage_commands import create_permission
+from discord_slash.model import SlashCommandPermissionType
 
 from ptn.boozebot.BoozeCarrier import BoozeCarrier
-from ptn.boozebot.constants import bot_guild_id, get_custom_assassin_id, bot, get_discord_booze_unload_channel
+from ptn.boozebot.constants import bot_guild_id, get_custom_assassin_id, bot, get_discord_booze_unload_channel, \
+    server_admin_role_id, server_carrier_owner_role_id, server_sommelier_role_id, server_aux_carrier_role_id
 from ptn.boozebot.database.database import carrier_db, carrier_db_lock, carriers_conn
 
 
@@ -13,9 +17,19 @@ class Unloading(commands.Cog):
         This class is a collection functionality for tracking a booze cruise unload operations
         """
 
-    @commands.has_any_role('Carrier Owner', 'Admin', 'Auxiliary Carrier')
-    @cog_ext.cog_slash(name="BoozeCruiseUnload", guild_ids=[bot_guild_id()],
-                       description="Creates a new unload operation in this channel.")
+    @cog_ext.cog_slash(
+        name="WineHelperMarketOpen",
+        guild_ids=[bot_guild_id()],
+        description="Creates a new unload operation in this channel.",
+        permissions={
+            bot_guild_id(): [
+                create_permission(server_admin_role_id(), SlashCommandPermissionType.ROLE, True),
+                create_permission(server_aux_carrier_role_id(), SlashCommandPermissionType.ROLE, True),
+                create_permission(server_carrier_owner_role_id(), SlashCommandPermissionType.ROLE, True),
+                create_permission(bot_guild_id(), SlashCommandPermissionType.ROLE, False),
+            ]
+        },
+    )
     async def booze_unload_market(self, ctx: SlashContext):
         """
         Command to set a booze cruise market unload. Generates a default message in the channel that it ran in.
@@ -38,10 +52,19 @@ class Unloading(commands.Cog):
         await message.add_reaction(f'<:Assassin:{str(get_custom_assassin_id())}>')
         await message.add_reaction('🍷')
 
-    @commands.has_any_role('Carrier Owner', 'Admin', 'Auxiliary Carrier')
-    @cog_ext.cog_slash(name="BoozeCruiseMarketClosed", guild_ids=[bot_guild_id()],
-                       description="Sends a dummy message to indicate you have closed your market. Command sent in "
-                                   "active channel.")
+    @cog_ext.cog_slash(
+        name="WineHelperMarketClosed",
+        guild_ids=[bot_guild_id()],
+        description="Sends a dummy message to indicate you have closed your market. Command sent in active channel.",
+        permissions={
+            bot_guild_id(): [
+                create_permission(server_admin_role_id(), SlashCommandPermissionType.ROLE, True),
+                create_permission(server_aux_carrier_role_id(), SlashCommandPermissionType.ROLE, True),
+                create_permission(server_carrier_owner_role_id(), SlashCommandPermissionType.ROLE, True),
+                create_permission(bot_guild_id(), SlashCommandPermissionType.ROLE, False),
+                ]
+            },
+        )
     async def booze_market_closed(self, ctx: SlashContext):
         print(f'User {ctx.author} requested a to close the market in channel: {ctx.channel}.')
         embed = discord.Embed(title='Batten Down The Hatches! This sale is currently done!')
@@ -51,9 +74,75 @@ class Unloading(commands.Cog):
         message = await ctx.send(embed=embed)
         await message.add_reaction('🏴‍☠️')
 
-    @commands.has_any_role('Carrier Owner', 'Admin', 'Sommelier')
-    @cog_ext.cog_slash(name='WineUnload', guild_ids=[bot_guild_id()],
-                       description='Posts a new wine unloading notification.')
+    @cog_ext.cog_slash(
+        name='WineUnload',
+        guild_ids=[bot_guild_id()],
+        description='Posts a new wine unloading notification for a specific carrier.',
+        permissions={
+            bot_guild_id(): [
+                create_permission(server_admin_role_id(), SlashCommandPermissionType.ROLE, True),
+                create_permission(server_sommelier_role_id(), SlashCommandPermissionType.ROLE, True),
+                create_permission(server_carrier_owner_role_id(), SlashCommandPermissionType.ROLE, True),
+                create_permission(bot_guild_id(), SlashCommandPermissionType.ROLE, False),
+            ]
+        },
+        options=[
+            create_option(
+                name='carrier_id',
+                description='The XXX-XXX ID string for the carrier',
+                option_type=3,
+                required=True
+            ),
+            create_option(
+                name='planetary_body',
+                description='A string representing the location of the carrier, ie Star, P1, P2',
+                option_type=3,
+                required=True,
+                choices=[
+                    create_choice(
+                        name="Star",
+                        value="Star"
+                    ),
+                    create_choice(
+                        name="Planet 1",
+                        value="Planet 1"
+                    ),
+                    create_choice(
+                        name="Planet 2",
+                        value="Planet 2"
+                    ),
+                    create_choice(
+                        name="Planet 3",
+                        value="Planet 3"
+                    ),
+                    create_choice(
+                        name="Planet 4",
+                        value="Planet 4"
+                    ),
+                    create_choice(
+                        name="Planet 5",
+                        value="Planet 5"
+                    ),
+                    create_choice(
+                        name="Planet 6",
+                        value="Planet 6"
+                    )
+                ]
+            ),
+            create_option(
+                name='timed_market',
+                description='True if carrier is running timed markets, else False',
+                option_type=5,
+                required=True
+            ),
+            create_option(
+                name='unload_channel',
+                description='The discord channel #xxx which the carrier will run timed unloads in',
+                option_type=3,
+                required=False
+            )
+        ]
+    )
     async def wine_carrier_unload(self, ctx: SlashContext, carrier_id, planetary_body, timed_market: bool,
                                   unload_channel=None):
         """
@@ -72,8 +161,8 @@ class Unloading(commands.Cog):
               f'channel: {unload_channel} using timed markets: {timed_market}.')
 
         if timed_market and not unload_channel:
-            return await ctx.send(f'Sorry, to run a timed market we need an unload channel, you '
-                                  f'provided: {unload_channel}.')
+            return await ctx.channel.send(f'Sorry, to run a timed market we need an unload channel, you '
+                                          f'provided: {unload_channel}.')
 
         carrier_db.execute(
             "SELECT * FROM boozecarriers WHERE carrierid LIKE (?)", (f'%{carrier_id}%',)
@@ -90,14 +179,14 @@ class Unloading(commands.Cog):
             unloading_channel_id = bot.get_channel(
                 int(unload_channel.replace('#', '').replace('<', '').replace('>', ''))
             )
-        print('{}')
+
         if carrier_data.discord_unload_notification:
             print(f'Sorry, carrier {carrier_data.carrier_identifier} is already on a wine unload.')
             return await ctx.send(f'Carrier: {carrier_data.carrier_name} ({carrier_data.carrier_identifier}) is '
                                   f'already unloading wine. Check the notification in {unload_channel}.')
 
         print(f'Starting to post un-load operation for carrier: {carrier_data}')
-        message_send = await ctx.send("**Sending to Discord...**")
+        message_send = await ctx.channel.send("**Sending to Discord...**")
 
         market_conditions = 'Timed Openings' if timed_market else f'{carrier_data.platform} Squadron and Friends'
 
@@ -148,13 +237,32 @@ class Unloading(commands.Cog):
             embed.set_footer(text='C/O: Try the commands /boozecruiseunload and /boozecruisemarketclosed.')
             await unloading_channel_id.send(embed=embed)
 
-        return await ctx.send(f'Wine unload requested by {ctx.author} for {carrier_id} processed successfully. '
-                              f'Market: {market_conditions}. {unload_tracking}'
-                              )
+        return await ctx.channel.send(
+            f'Wine unload requested by {ctx.author} for {carrier_id} processed successfully. Market: '
+            f'{market_conditions}. {unload_tracking}'
+        )
 
-    @commands.has_any_role('Carrier Owner', 'Admin', 'Sommelier')
-    @cog_ext.cog_slash(name='WineUnloadComplete', guild_ids=[bot_guild_id()],
-                       description='Removes any trade channel notification for unloading wine.')
+    @cog_ext.cog_slash(
+        name='WineUnloadComplete',
+        guild_ids=[bot_guild_id()],
+        description='Removes any trade channel notification for unloading wine.',
+        options=[
+            create_option(
+                name='carrier_id',
+                description='The XXX-XXX ID string for the carrier',
+                option_type=3,
+                required=True
+            )
+        ],
+        permissions={
+            bot_guild_id(): [
+                create_permission(server_admin_role_id(), SlashCommandPermissionType.ROLE, True),
+                create_permission(server_sommelier_role_id(), SlashCommandPermissionType.ROLE, True),
+                create_permission(server_carrier_owner_role_id(), SlashCommandPermissionType.ROLE, True),
+                create_permission(bot_guild_id(), SlashCommandPermissionType.ROLE, False),
+            ]
+        },
+    )
     async def wine_unloading_complete(self, ctx: SlashContext, carrier_id):
         print(f'Wine unloading complete for {carrier_id} flagged by {ctx.author}.')
 
@@ -167,6 +275,7 @@ class Unloading(commands.Cog):
         if not carrier_data:
             return await ctx.send(f'Sorry, could not find a carrier for the data: {carrier_id}.')
 
+        response = None
         if carrier_data.discord_unload_notification and carrier_data.discord_unload_notification != 'NULL':
             # If we have a notification, remove it.
             print(f'Deleting the wine carrier unload notification for: {carrier_id}.')
@@ -194,4 +303,4 @@ class Unloading(commands.Cog):
                        f'{carrier_id}.'
             print(f'No discord alert found for carrier, {carrier_id}. It likely ran open market.')
 
-        return await ctx.send(response)
+        return await ctx.channel.send(content=response)
