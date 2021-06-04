2e47b9c5a082fa363bef7faadb933a1533526c1a
gcruics <53232597+gcruicks@users.noreply.github.com>
2021-06-04 09:51:13 +0200

    Add an entry script for the bot
    Added entry script
    Fixes #55

--- ptn/boozebot/booze.py
+++ ptn/boozebot/booze.py
@@ -6,9 +6,19 @@ from ptn.boozebot.database.database import build_database_on_startup
 
 print(f'Booze bot is connecting against production: {_production}.')
 
-if __name__ == '__main__':
+
+def run():
+    """
+    Logic to build the bot and run the script.
+
+    :returns: None
+    """
     build_database_on_startup()
     bot.add_cog(DiscordBotCommands(bot))
     bot.add_cog(Unloading())
     bot.add_cog(DatabaseInteraction())
     bot.run(TOKEN)
+
+
+if __name__ == '__main__':
+    run()
