c07c5d02c8babd6846ca626cedd438fd5e4fe74e
gcruics <53232597+gcruicks@users.noreply.github.com>
2021-06-05 16:28:43 +0200

    Add a help command that dumps data on what the commands do
    Add somle help command

--- ptn/boozebot/booze.py
+++ ptn/boozebot/booze.py
@@ -1,5 +1,6 @@
 from ptn.boozebot.commands.DatabaseInteraction import DatabaseInteraction
 from ptn.boozebot.commands.DiscordBotCommands import DiscordBotCommands
+from ptn.boozebot.commands.Helper import Helper
 from ptn.boozebot.commands.Unloading import Unloading
 from ptn.boozebot.constants import bot, TOKEN, _production
 from ptn.boozebot.database.database import build_database_on_startup
@@ -17,6 +18,7 @@ def run():
     bot.add_cog(DiscordBotCommands(bot))
     bot.add_cog(Unloading())
     bot.add_cog(DatabaseInteraction())
+    bot.add_cog(Helper())
     bot.run(TOKEN)
 
 
