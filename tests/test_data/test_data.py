36eecf751e43323a1716cc50b33c5bfece45e4fb
chandru <chandru9279@users.noreply.github.com>
2022-06-26 00:15:31 +0530

    feedback considered

--- tests/test_data/test_data.py
+++ tests/test_data/test_data.py
@@ -14,22 +14,21 @@ class MockResponse:
 
 dirname = os.path.dirname(__file__)
 
-with open(f'{dirname}/peak_faction_holiday_response.json', mode='r') as data:
-    holiday_response = data.read()
 
-with open(f'{dirname}/peak_faction_workingday_response.json', mode='r') as data:
-    workingday_response = data.read()
+def load_response(filename):
+    with open(f'{dirname}/{filename}.json', mode='r') as data:
+        return data.read()
 
 
 def mocked_requests_holiday_response(*args, **kwargs):
-    if args[0] == 'https://elitebgs.app/api/ebgs/v5/factions' \
-            and kwargs['params']['name'] == 'Rackham Capital Investments':
-        return MockResponse(holiday_response, 200)
+    if args[0]=='https://elitebgs.app/api/ebgs/v5/factions' \
+            and kwargs['params']['name']=='Rackham Capital Investments':
+        return MockResponse(load_response("peak_faction_holiday_response"), 200)
     return MockResponse(None, 404)
 
 
-def mocked_requests_workingday_response(*args, **kwargs):
-    if args[0] == 'https://elitebgs.app/api/ebgs/v5/factions' \
-            and kwargs['params']['name'] == 'Rackham Capital Investments':
-        return MockResponse(workingday_response, 200)
+def mocked_requests_no_holiday_response(*args, **kwargs):
+    if args[0]=='https://elitebgs.app/api/ebgs/v5/factions' \
+            and kwargs['params']['name']=='Rackham Capital Investments':
+        return MockResponse(load_response("peak_faction_no_holiday_response"), 200)
     return MockResponse(None, 404)
