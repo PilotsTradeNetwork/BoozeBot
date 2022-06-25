455d30e26fa0d1cc4ebcb1a0f7d92fb709e50a95
chandru <chandru9279@users.noreply.github.com>
2022-06-26 00:15:31 +0530

    feedback considered

--- tests/test_ph_check.py
+++ tests/test_ph_check.py
@@ -12,6 +12,6 @@ class PublicHolidayChecks(unittest.TestCase):
     def test_ph_check_holiday(self, mock_request_get):
         self.assertTrue(ph_check())
 
-    @mock.patch('requests.get', side_effect=test_data.mocked_requests_workingday_response)
+    @mock.patch('requests.get', side_effect=test_data.mocked_requests_no_holiday_response)
     def test_ph_check(self, mock_request_get):
         self.assertFalse(ph_check())
