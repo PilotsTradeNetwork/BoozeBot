f4ee527c19053fb02a0944ef49f0eeef12d11e69
chandru <chandru9279@users.noreply.github.com>
2022-06-26 22:59:38 +0530

    feedback considered - part 2

--- tests/test_data/test_data.py
+++ tests/test_data/test_data.py
@@ -24,11 +24,9 @@ def mocked_requests_holiday_response(*args, **kwargs):
     if args[0]=='https://elitebgs.app/api/ebgs/v5/factions' \
             and kwargs['params']['name']=='Rackham Capital Investments':
         return MockResponse(load_response("peak_faction_holiday_response"), 200)
-    return MockResponse(None, 404)
 
 
 def mocked_requests_no_holiday_response(*args, **kwargs):
     if args[0]=='https://elitebgs.app/api/ebgs/v5/factions' \
             and kwargs['params']['name']=='Rackham Capital Investments':
         return MockResponse(load_response("peak_faction_no_holiday_response"), 200)
-    return MockResponse(None, 404)
