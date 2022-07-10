0902390317786e8cf15ad57e79fb492a2fb04f9f
gcruics <53232597+gcruicks@users.noreply.github.com>
2022-07-10 17:43:20 +0200

    PR-324 - Add wine tanker unload commands
    Add wine tanker unloading commands.

--- ptn/boozebot/commands/Unloading.py
+++ ptn/boozebot/commands/Unloading.py
@@ -10,7 +10,8 @@ from discord_slash.model import SlashCommandPermissionType
 from ptn.boozebot.BoozeCarrier import BoozeCarrier
 from ptn.boozebot.constants import bot_guild_id, get_custom_assassin_id, bot, get_discord_booze_unload_channel, \
     server_admin_role_id, server_sommelier_role_id, server_wine_carrier_role_id, \
-    server_mod_role_id, get_primary_booze_discussions_channel, get_fc_complete_id, server_wine_tanker_role_id
+    server_mod_role_id, get_primary_booze_discussions_channel, get_fc_complete_id, server_wine_tanker_role_id, \
+    get_wine_tanker_role, get_discord_tanker_unload_channel
 from ptn.boozebot.database.database import pirate_steve_db, pirate_steve_lock, pirate_steve_conn
 
 
@@ -253,7 +254,7 @@ class Unloading(commands.Cog):
                         f'*{planetary_body}**.'
                         f'\n Market Conditions: **{market_conditions}**.{unload_tracking}'
         )
-        fc_complete = bot.get_emoji(get_fc_complete_id())
+
         wine_load_embed.set_footer(text=f'Please react with this emoji once completed.',
                                    icon_url=f'https://cdn.discordapp.com/emojis/{get_fc_complete_id()}.png?v=1')
         wine_unload_alert = await wine_alert_channel.send(embed=wine_load_embed)
@@ -449,3 +450,122 @@ class Unloading(commands.Cog):
             except Exception as e:
                 print(e)
                 await ctx.send(f"Failed adding role {set_role} to {user}: {e}")
+
+    @cog_ext.cog_slash(
+        name='tanker_unload',
+        guild_ids=[bot_guild_id()],
+        description='Posts a new tanker unload notice for a carrier. Admin/Sommelier/WineTanker role required.',
+        permissions={
+            bot_guild_id(): [
+                create_permission(server_admin_role_id(), SlashCommandPermissionType.ROLE, True),
+                create_permission(server_sommelier_role_id(), SlashCommandPermissionType.ROLE, True),
+                create_permission(server_mod_role_id(), SlashCommandPermissionType.ROLE, True),
+                create_permission(get_wine_tanker_role(), SlashCommandPermissionType.ROLE, True),
+                create_permission(bot_guild_id(), SlashCommandPermissionType.ROLE, False),
+            ]
+        },
+        options=[
+            create_option(
+                name='carrier_id',
+                description='The XXX-XXX ID string for the carrier',
+                option_type=3,
+                required=True
+            ),
+            create_option(
+                name='system_name',
+                description='The system the carrier is present in.',
+                option_type=3,
+                required=True
+            ),
+            create_option(
+                name='planetary_body',
+                description='A string representing the location of the carrier, ie Star, P1, P2',
+                option_type=3,
+                required=True,
+            ),
+        ]
+    )
+    async def wine_tanker_unload(self, ctx: SlashContext, carrier_id: str, system_name: str, planetary_body: str):
+        """
+        Tanker unload command.
+
+        :param SlashContext ctx: The discord message context.
+        :param str carrier_id: The carrier ID as a string (XXX-XXX).
+        :param str system_name: The system the unload is in.
+        :param str planetary_body: The planetary body the carrier is located at.
+        :returns: None
+        """
+        print(f'User {ctx.author} has requested a new tanker unload operation for carrier: {carrier_id} around the '
+              f'body: {planetary_body} in system: "{system_name}".')
+
+        # Cast this to upper case just in case
+        carrier_id = carrier_id.upper()
+
+        # Check the carrier ID regex
+        if not re.match(r"\w{3}-\w{3}", carrier_id):
+            print(f'{ctx.author}, the carrier ID was invalid, XXX-XXX expected received, {carrier_id}.')
+            return await ctx.channel.send(f'{ctx.author}, the carrier ID was invalid during tanker unload, '
+                                          f'XXX-XXX expected received, {carrier_id}.')
+
+        pirate_steve_lock.acquire()
+        pirate_steve_db.execute(
+            "SELECT * FROM boozecarriers WHERE carrierid LIKE (?)", (f'%{carrier_id}%',)
+        )
+
+        # We will only get a single entry back here as the carrierid is a unique field.
+        carrier_data = BoozeCarrier(pirate_steve_db.fetchone())
+        pirate_steve_lock.release()
+
+        if not carrier_data:
+            print(f'We failed to find the carrier: {carrier_id} in the database.')
+            return await ctx.send(f'Sorry, during unload we could not find a carrier for the data: {carrier_id}.')
+
+        tanker_unload_channel = bot.get_channel(get_discord_tanker_unload_channel())
+        if carrier_data.discord_unload_notification:
+            print(f'Sorry, carrier {carrier_data.carrier_identifier} is already on a wine unload.')
+            return await ctx.send(f'Carrier: {carrier_data.carrier_name} ({carrier_data.carrier_identifier}) is '
+                                  f'already unloading wine. No second notification posted.')
+
+        tanker_embed = discord.Embed(
+            title='Wine unload notification.',
+            description=f'Tanker {carrier_data.carrier_name} (**{carrier_data.carrier_identifier}**) is currently '
+                        f'unloading **{carrier_data.wine_total // carrier_data.run_count}** tonnes of wine in '
+                        f'**system: {system_name}** from **{planetary_body}**.'
+        )
+        fc_complete = bot.get_emoji(get_fc_complete_id())
+        tanker_embed.set_footer(text=f'Please react with this emoji once completed.',
+                                icon_url=f'https://cdn.discordapp.com/emojis/{get_fc_complete_id()}.png?v=1')
+        tanker_unload_alert = await tanker_unload_channel.send(embed=tanker_embed)
+
+        # Get the discord alert ID and drop it into the database
+        discord_alert_id = tanker_unload_alert.id
+
+        print(f'Posted the tanker unload alert for {carrier_data.carrier_name} ({carrier_data.carrier_identifier})')
+
+        try:
+            pirate_steve_lock.acquire()
+            data = (
+                discord_alert_id,
+                f'%{carrier_id}%'
+            )
+
+            pirate_steve_db.execute('''
+                UPDATE boozecarriers
+                SET discord_unload_in_progress=?, totalunloads=totalunloads+1
+                WHERE carrierid LIKE (?)
+            ''', data)
+            pirate_steve_conn.commit()
+        finally:
+            pirate_steve_lock.release()
+        print(f'Discord alert ID written to database for {carrier_data.carrier_identifier}')
+
+        # Also post a note into the primary channel to go read the announcements.
+        booze_cruise_chat = bot.get_channel(get_primary_booze_discussions_channel())
+        await booze_cruise_chat.send(f"A new wine unload is in progress. See <#{tanker_unload_alert.channel.id}>")
+
+        return await ctx.send(
+            f'Wine unload requested by {ctx.author} for **{carrier_data.carrier_name}** ({carrier_id}) '
+            f'processed successfully. Unloading in **system: {system_name}** at **position: {planetary_body}**.'
+        )
+
+    # TODO: Make a separate unload command once we know what the tracking process will be.
