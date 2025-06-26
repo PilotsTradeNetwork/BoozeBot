f3bb8788294cdf281d8b3bbeee0dcc41e34b06c9
Conshmea <77846041+Conshmea@users.noreply.github.com>
2025-06-26 13:00:56 +0100

    Make ph_check check the database

--- tests/test_ph_check.py
+++ tests/test_ph_check.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest import mock
 
-from ptn.boozebot.modules.PHcheck import ph_check
+from ptn.boozebot.modules.PHcheck import api_ph_check
 from tests.test_data import test_data
 
 
@@ -10,8 +10,8 @@ class PublicHolidayChecks(unittest.TestCase):
 
     @mock.patch('requests.get', side_effect=test_data.mocked_requests_holiday_response)
     async def test_ph_check_holiday(self, _mock_request_get):
-        self.assertTrue(await ph_check())
+        self.assertTrue(await api_ph_check())
 
     @mock.patch('requests.get', side_effect=test_data.mocked_requests_no_holiday_response)
     async def test_ph_check(self, _mock_request_get):
-        self.assertFalse(await ph_check())
+        self.assertFalse(await api_ph_check())
