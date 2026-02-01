343bbb91546eee290a1c21e271ad38743e3b28b2
CMDR Axle Antilles <axleantilles@proton.me>
2026-02-01 15:11:34 -0800

    move unowned carrier error higher

--- ptn/boozebot/botcommands/Unloading.py
+++ ptn/boozebot/botcommands/Unloading.py
@@ -313,6 +313,11 @@ class Unloading(commands.Cog):
                 content=f"Sorry, during unload we could not find a carrier for the data: {carrier_id}."
             )
 
+        if not carrier_data.is_owned_by(interaction.user) and not is_staff(interaction.user):
+            msg = f"You do not own the carrier with ID: {carrier_id}."
+            logger.info(msg)
+            return await interaction.edit_original_response(content=msg)
+
         if carrier_data.system != "N0":
             msg = f"Carrier {carrier_data.carrier_identifier} is not in N0 (HIP 58832); cannot unload wine."
             logger.info(msg)
@@ -323,11 +328,6 @@ class Unloading(commands.Cog):
             return await interaction.edit_original_response(
                 content=f"Sorry, you must first set a body for your carrier {carrier_id} in the Wine Carrier Sheet before unloading."
             )
-
-        if not carrier_data.is_owned_by(interaction.user) and not is_staff(interaction.user):
-            msg = f"You do not own the carrier with ID: {carrier_id}."
-            logger.info(msg)
-            return await interaction.edit_original_response(content=msg)
 
         wine_alert_channel = await bot.get_or_fetch.channel(CHANNEL_BC_WINE_CELLAR_UNLOADING)
 
