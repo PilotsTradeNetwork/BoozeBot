1486d799b57200905577957f1203963bb7b789db
CMDR Axle Antilles <145315964+axleantilles@users.noreply.github.com>
2026-02-20 11:51:25 -0800

    Remove logging of wine carrier members
    Removed logging of wine carrier members before role removal.

--- ptn/boozebot/botcommands/Cleaner.py
+++ ptn/boozebot/botcommands/Cleaner.py
@@ -261,9 +261,7 @@ class Cleaner(commands.Cog):
             wine_carrier_role = await bot.get_or_fetch.role(ROLE_WINE_CARRIER)
             hitch_role = await bot.get_or_fetch.role(ROLE_HITCHHIKER)
             ptnrpphtms_role = await bot.get_or_fetch.role(ROLE_PTNRPPHTMS)
-
-            logger.info(f"wine carriers: {wine_carrier_role.members}")
-
+            
             await interaction.edit_original_response(
                 content="Removing roles, This may take a minute...", embed=None, view=None
             )
