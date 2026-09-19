7f0a6067195f366f35214f8121fd4ff5ae1bb517
Conshmea <77846041+Conshmea@users.noreply.github.com>
2026-09-19 16:31:33 +0100

    Post upcoming timers

--- ptn/boozebot/constants.py
+++ ptn/boozebot/constants.py
@@ -101,6 +101,7 @@ class Settings(BotSettings):
         "periodic_signup_poll": True,
         "boozesheets_websocket": True,
         "boozesheets_carrier_poll": True,
+        "upcoming_timers_loop": True,
     }
 
 
